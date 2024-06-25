4fd031e24551416b2de78ef0cdf8c10a1675d4d8
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-06-25 04:56:24 +0100

    created stripe payment checkout session

--- src/app/configure/preview/actions.ts
+++ src/app/configure/preview/actions.ts
@@ -1,6 +1,7 @@
 "use server";
 import { BASE_PRICE, PRODUCT_PRICES } from "@/config/products";
 import { db } from "@/db";
+import { stripe } from "@/lib/stripe";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { Order } from "@prisma/client";
 
@@ -54,4 +55,29 @@ export const createCheckoutSession = async ({
       configurationId: configuration.id,
     },
   });
+
+  const product = await stripe.products.create({
+    name: "Custom iPhone Case",
+    images: [configuration.imageUrl],
+    default_price_data: {
+      currency: "USD",
+      unit_amount: price,
+    },
+  });
+
+  // payment session
+  const stripeSession = await stripe.checkout.sessions.create({
+    success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId=${order.id}`,
+    cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/configure/preview?id=${configuration.id}`,
+    payment_method_types: ["card", "paypal", "cashapp"],
+    mode: "payment",
+    // shipping_address_collection: {}
+    metadata: {
+      userId: user.id,
+      orderId: order.id,
+    },
+    line_items: [{ price: product.default_price as string, quantity: 1 }],
+  });
+
+  return { url: stripeSession.url };
 };
